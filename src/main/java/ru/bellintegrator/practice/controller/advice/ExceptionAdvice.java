855295c71d7e720936ced674964706ee4a4a9c4e
Aleksey Avetisian <s1t3w4n@yandex.ru>
2020-12-21 18:01:02 +0300

    Добавлено логгирование ошибки с uuid, изменено сообщение

--- src/main/java/ru/bellintegrator/practice/controller/advice/ExceptionAdvice.java
+++ src/main/java/ru/bellintegrator/practice/controller/advice/ExceptionAdvice.java
@@ -1,5 +1,7 @@
 package ru.bellintegrator.practice.controller.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,23 +10,32 @@ import ru.bellintegrator.practice.exception.EntityNotFoundException;
 import ru.bellintegrator.practice.exception.WrongDataException;
 import ru.bellintegrator.practice.view.global.ErrorView;
 
+import java.util.UUID;
+
 /**
  * Преобразователь всех ответов, которые возвращают исключение
  */
 @RestControllerAdvice
 public class ExceptionAdvice {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    private static final String INTERNAL_SERVER_ERROR = "Внутрення ошибка сервера";
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorView> exception(Exception exception) {
+        String evaluatedMessage = exception.getMessage() + " UUID: " + UUID.randomUUID();
+        LOGGER.warn(evaluatedMessage, exception);
         if (exception instanceof EntityNotFoundException) {
-            return makeErrorResponse(exception, HttpStatus.NOT_FOUND);
+            return makeErrorResponse(evaluatedMessage, HttpStatus.NOT_FOUND);
         } else if (exception instanceof WrongDataException) {
-            return makeErrorResponse(exception, HttpStatus.BAD_REQUEST);
+            return makeErrorResponse(evaluatedMessage, HttpStatus.BAD_REQUEST);
         } else {
-            return makeErrorResponse(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+            return makeErrorResponse(INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private ResponseEntity<ErrorView> makeErrorResponse(Exception exception, HttpStatus status) {
-        return new ResponseEntity<>(new ErrorView(exception.getMessage()), status);
+    private ResponseEntity<ErrorView> makeErrorResponse(String message, HttpStatus status) {
+        return new ResponseEntity<>(new ErrorView(message), status);
     }
 }
--- src/main/java/ru/bellintegrator/practice/view/global/ErrorView.java
+++ src/main/java/ru/bellintegrator/practice/view/global/ErrorView.java
@@ -2,7 +2,6 @@ package ru.bellintegrator.practice.view.global;
 
 import lombok.Data;
 
-import java.util.UUID;
 /**
  * View ответа в который оборачиваются все ошибки при отправке по api
  */
@@ -13,7 +12,4 @@ public class ErrorView {
      */
     private final String error;
 
-    public ErrorView(String message) {
-        this.error = message + " UUID: " + UUID.randomUUID();
-    }
 }
